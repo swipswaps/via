4f0db0798e6637df900b47557a53288bb557ee3c
Sheetal Umesh Kumar <sheetaluk@hypothes.is>
2018-06-26 10:12:37 -0400

    Block youtube and vimeo because proxying large videos through via is causing auto scaling issues and via also to stop responding.
    Also, strip schemes from the url to annotate, so that we can ignore http and https
    variations of the same domains while checking for blocked domains.
    
    See https://github.com/hypothesis/product-backlog/issues/626

--- via/blocker.py
+++ via/blocker.py
@@ -5,37 +5,46 @@ import os
 from jinja2 import Environment, FileSystemLoader
 from werkzeug import wsgi
 from werkzeug.wrappers import BaseResponse as Response
+from urlparse import urlparse
 
 TEMPLATES_DIR = os.path.dirname(os.path.abspath(__file__)) + '/../templates/'
-PREFIXES = [{"prefix": "/http://nautil.us", "template": "disallow_access.html.jinja2", "status": 451},
-            {"prefix": "/http://m.nautil.us", "template": "disallow_access.html.jinja2", "status": 451},
-            {"prefix": "/http://m.youtube.com", "template": "could_not_process.html.jinja2", "status": 200},
-            {"prefix": "/https://www.youtube.com", "template": "could_not_process.html.jinja2", "status": 200},
-            {"prefix": "/http://vimeo.com", "template": "could_not_process.html.jinja2", "status": 200}]
+DOMAINS = [{"domain": "nautil.us", "template": "disallow_access.html.jinja2", "status": 451},
+           {"domain": "m.nautil.us", "template": "disallow_access.html.jinja2", "status": 451},
+           {"domain": "m.youtube.com", "template": "could_not_process.html.jinja2", "status": 200},
+           {"domain": "www.youtube.com", "template": "could_not_process.html.jinja2", "status": 200},
+           {"domain": "vimeo.com", "template": "could_not_process.html.jinja2", "status": 200}]
 
 
 class Blocker(object):
 
     """
     Blocker is a WSGI middleware that returns a static response when a
-    request path matches a list of predefined prefixes.
+    request path matches a list of predefined domains.
     """
 
-    def __init__(self, application, prefixes=None, template=None):
+    def __init__(self, application, domains=None, template=None):
         self._application = application
         self._jinja_env = Environment(loader=FileSystemLoader(TEMPLATES_DIR),
                                       trim_blocks=True)
-        self._prefixes = prefixes or PREFIXES
+        self._domains = domains or DOMAINS
         self._template = template
 
     def __call__(self, environ, start_response):
-        for _p in self._prefixes:
-            url_to_annotate = wsgi.get_path_info(environ)
-            if url_to_annotate.startswith(_p.get('prefix')):
+        url_to_annotate = wsgi.get_path_info(environ)[1:]
+        parsed_url = urlparse(url_to_annotate)
+
+        if not parsed_url.scheme:
+            url_to_annotate = "http://" + url_to_annotate
+            parsed_url = urlparse(url_to_annotate)
+
+        hostname_to_annotate = parsed_url.hostname
+
+        for _d in self._domains:
+            if hostname_to_annotate and hostname_to_annotate == _d['domain']:
                 template = self._template
-                if _p.get('template'):
-                    template = self._jinja_env.get_template(_p.get('template')).render(url_to_annotate=url_to_annotate[1:])
+                if _d.get('template'):
+                    template = self._jinja_env.get_template(_d.get('template')).render(url_to_annotate=url_to_annotate)
                 resp = Response(template,
-                                status=_p['status'], mimetype='text/html')
+                                status=_d['status'], mimetype='text/html')
                 return resp(environ, start_response)
         return self._application(environ, start_response)
