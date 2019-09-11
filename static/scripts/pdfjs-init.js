3878ad5f9a6d56c247809889ad6bc2329c5bfe89
Robert Knight <robertknight@gmail.com>
2019-09-11 15:27:41 +0100

    Load the Hypothesis client sooner
    Load the Hypothesis client as soon as the PDF.js viewer is initialized,
    rather than waiting for the PDF itself to be loaded.
    
    The client currently requires the PDF viewer to be initialized before it
    is loaded, but it already has logic to wait for the PDF itself to be
    loaded where necessary.
    
    Loading the client sooner reduces the overall time until annotations are
    visible and matches the behavior of Via with PDF.js v1.

--- static/scripts/pdfjs-init.js
+++ static/scripts/pdfjs-init.js
@@ -2,11 +2,23 @@
 
 // Note: This file is not transpiled. For IE 11 compatibility, it must only
 // use ES5 language features.
+//
+// This script can however assume that some ES2015+ library features, eg.
+// Promises, will already have been polyfilled as PDF.js uses them.
 
 // Listen for `webviewerloaded` event to configure the viewer after its files
 // have been loaded but before it is initialized.
 document.addEventListener('webviewerloaded', function(event) {
   var appOptions = window.PDFViewerApplicationOptions;
+  var app = window.PDFViewerApplication;
+
+  // Ensure that PDF.js viewer events such as "documentloaded" are dispatched
+  // to the DOM. The client relies on this.
+  appOptions.set('eventBusDispatchToDOM', true);
+
+  // Disable preferences support, as otherwise this will result in `eventBusDispatchToDOM`
+  // being overridden with the default value of `false`.
+  appOptions.set('disablePreferences', true);
 
   // Read configuration rendered into template as global vars.
   var url = VIA_DOCUMENT_URL;
@@ -15,10 +27,7 @@ document.addEventListener('webviewerloaded', function(event) {
   // Load the PDF specified in the URL.
   //
   // See https://github.com/mozilla/pdf.js/wiki/Frequently-Asked-Questions#can-i-specify-a-different-pdf-in-the-default-viewer and https://github.com/mozilla/pdf.js/issues/10435#issuecomment-452706770
-  //
-  // Prevent loading of the default PDF bundled with the viewer.
-  appOptions.set('defaultUrl', '');
-  var app = window.PDFViewerApplication;
+  appOptions.set('defaultUrl', ''); // Prevent loading of default viewer PDF.
   app.open({
     // Load PDF through Via to work around CORS restrictions.
     url: '/id_/' + url,
@@ -26,8 +35,28 @@ document.addEventListener('webviewerloaded', function(event) {
     // Make sure `PDFViewerApplication.url` returns the original URL, as this
     // is the URL associated with annotations.
     originalUrl: url,
-  }).then(function () {
-    // Once the PDF has been loaded, then inject the client.
+  });
+
+  // Wait for the PDF viewer to be fully initialized and then load the Hypothesis client.
+  //
+  // This is required because the client currently assumes that `PDFViewerApplication`
+  // is fully initialized when it loads. Note that "fully initialized" only means
+  // that the PDF viewer application's components have been initialized. The
+  // PDF itself will still be loading, and the client will wait for that to
+  // complete before fetching annotations.
+  //
+  var pdfjsInitialized = new Promise(function (resolve) {
+    // Poll `app.initialized` as there doesn't appear to be an event that
+    // we can listen to.
+    var timer = setInterval(function () {
+      if (app.initialized) {
+        clearTimeout(timer);
+        resolve();
+      }
+    }, 20);
+  });
+
+  pdfjsInitialized.then(function () {
     var embedScript = document.createElement('script');
     embedScript.src = clientEmbedUrl;
     document.body.appendChild(embedScript);
