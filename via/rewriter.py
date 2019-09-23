35cbe1a7a220e614ecb6d8132ba79f5e3b47a067
Robert Knight <robertknight@gmail.com>
2019-09-23 14:19:16 +0100

    Disable most media and script URL rewriting
    URL rewriting significantly slows down page loads due to the cost of
    proxying files through a Python app in our US-based servers compared to loading
    them from eg. a CDN local to the user. It also significantly increases
    the volume of traffic that the Via service has to handle.
    
    As part of an investigation into finding out how little URL rewriting we
    can get away with and still have a useful service, override pywb's
    default URL rewriter to disable rewriting of:
    
     - URLs which are very likely to be media files
     - URLs which are very likely to be scripts
     - URLs which refer to other websites than the one being proxied
     - iframes

--- via/rewriter.py
+++ via/rewriter.py
@@ -1,9 +1,12 @@
 from __future__ import unicode_literals
 
+from urlparse import urlparse, urljoin
+
 import cgi
 import jinja2
 
 from pywb.webapp.live_rewrite_handler import RewriteHandler
+from pywb.rewrite.url_rewriter import UrlRewriter as BaseUrlRewriter
 from pywb.framework.wbrequestresponse import WbResponse
 
 
@@ -37,6 +40,90 @@ class TemplateRewriteHandler(RewriteHandler):
         return super(TemplateRewriteHandler, self)._make_response(
             wbrequest, status_headers, gen, is_rewritten
         )
+
+
+class UrlRewriter(BaseUrlRewriter):
+    """
+    URL rewriter invoked by pywb to rewrite references to URLs in proxied pages.
+
+    The base implementation in pywb rewrites all URLs to go through the proxy.
+
+    Every URL that is proxied adds overhead to the Via service and also slows
+    down the page load for the user, as resources are much slower if served
+    through the proxy than a local CDN.
+
+    :param url: The URL string to rewrite. This may be absolute or relative.
+    :param mod: A hint about the context in which the URL is used
+    """
+    def rewrite(self, url, mod=None):
+        # `url` may be absolute or relative. Convert to absolute if necessary.
+        if _is_absolute_url(url):
+            abs_url = url
+        else:
+            abs_url = urljoin(self.wburl.url, url)
+
+        # Don't proxy resources loaded as media files or that appear to be media
+        # files based on their extension.
+        if mod in ["im_", "oe_"] or _has_extension(abs_url, MEDIA_EXTENSIONS):
+            return abs_url
+
+        # Don't proxy iframes.
+        if mod in ["if_"]:
+            return abs_url
+
+        # Don't proxy scripts.
+        if mod in ["js_"] or _has_extension(abs_url, SCRIPT_EXTENSIONS):
+            return abs_url
+
+        # Don't rewrite URLs that refer to other origins.
+        if _url_origin(abs_url) != _url_origin(self.wburl.url):
+            return abs_url
+
+        # Rewrite the URL, which will make it go through the proxy.
+        return super(UrlRewriter, self).rewrite(url, mod)
+
+
+MEDIA_EXTENSIONS = [
+    # Image files
+    b".gif",
+    b".jpg",
+    b".jpeg",
+    b".ico",
+    b".png",
+    b".svg",
+
+    # Video files
+    b".webm",
+
+    # Audio files
+    b".mp3",
+    b".mp4",
+
+    # Fonts
+    b".woff",
+    b".woff2",
+]
+
+SCRIPT_EXTENSIONS = [b".js"]
+
+
+def _has_extension(url, extensions):
+    parsed_url = urlparse(url)
+    for ext in extensions:
+        if parsed_url.path.endswith(ext):
+            return True
+    return False
+
+
+def _url_origin(url):
+    parsed_url = urlparse(url)
+    return parsed_url.netloc
+
+
+def _is_absolute_url(url):
+    return (
+        url.startswith(b"http:") or url.startswith(b"https:") or url.startswith(b"//")
+    )
 
 
 def _lookup_key(val):
