64945f4dbaad0af2e95c33ed872a36aea4587b60
Robert Knight <robertknight@gmail.com>
2019-12-13 15:51:06 +0000

    Explicitly import the `via.rewriter` module
    This module is referenced by 'config.yaml' which pywb tries to load when
    imported.
    
    In the current version of pyyaml, the module must be imported explicitly
    before it can be referenced from YAML files.

--- via/__init__.py
+++ via/__init__.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 
+# This module is not used directly but is referenced by config.yaml.
+# This must be imported before `pywb` is imported in `via.app`.
+import via.rewriter  # noqa: F401
 from via._version import get_version
 
 __all__ = ("__version__",)
